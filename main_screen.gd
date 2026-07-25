404edba1dc082a3d1702188788f7b25bf92a1f77
Violette BAR <bar.violette@gmail.com>
2026-07-25 15:59:37 +0200

    Addind rocket launch mechanic

--- main_screen.gd
+++ main_screen.gd
@@ -4,6 +4,8 @@ extends Node2D
 
 var main_timer:Timer;
 
+var list_space_scenes = []
+
 var space_screen_scene = preload("res://space_screen.tscn")
 
 
@@ -21,8 +23,17 @@ func _process(delta: float) -> void:
 
 func add_screen() -> void:
 	var scene = space_screen_scene.instantiate()
+	list_space_scenes.append(scene)
 	add_child(scene)
 	main_timer.timeout.connect(scene.countdown)
 	print("on ajoute une scene")
 	
 	pass
+
+
+func _unhandled_input(event):
+	if event is InputEventKey:
+		if event.pressed and event.keycode == KEY_SPACE:
+			for scene in list_space_scenes:
+				scene.space_key_pressed()
+			
--- space_screen.gd
+++ space_screen.gd
@@ -1,6 +1,10 @@
 extends Node2D
 
 var count:int;
+
+var is_launched = false
+var is_crashed = true
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	count = 5;
@@ -12,7 +16,19 @@ func _process(delta: float) -> void:
 	pass
 
 func countdown() -> void:
-	print("je descend de 1 !, reste : ", count)
-	count -=1;
-	print(self.get_instance_id())
-	pass
+	if is_launched == false:
+		count -=1;
+		$Countdown_container/Countdown_label.text = str(count)
+		print("je descend de 1 !, reste : ", count)
+		print(self.get_instance_id())
+		
+		if count < 0:
+			is_crashed = true
+			$Countdown_container/Countdown_label.text = "Crashed !"
+
+func space_key_pressed():
+	print("scene received space")
+	if count == 0:
+		$Countdown_container/Countdown_label.text = "Launched !"
+		is_launched = true
+		
