de2712475285ed0384f7e19ed0dbf9bd615d6535
Thomas-Casadevall <46565613+Thomas-Casadevall@users.noreply.github.com>
2026-07-25 18:16:14 +0200

    add time window

--- main_screen.gd
+++ main_screen.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 #var screens: Array[Node2D]
 
+var TOTAL_TIME:float = 5;
+
 var main_timer:Timer;
 
 var fenetre:float = 0.1;
@@ -14,6 +16,7 @@ var space_screen_scene = preload("res://space_screen.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	main_timer = get_node("main_timer")
+	#GlobalSignalHandler.rocket_exploded.connect(on_rocket_crashed)
 	add_screen()
 	pass
 
@@ -24,9 +27,10 @@ func _process(delta: float) -> void:
 
 func add_screen() -> void:
 	var scene = space_screen_scene.instantiate()
-	GlobalSignalHandler.connect("on_rocket_launch", on_rocket_launch)
+	#GlobalSignalHandler.connect("on_rocket_launch", on_rocket_launch)
+	scene.rocket_exploded.connect(on_rocket_crashed)
 	list_space_scenes.append(scene)
-	
+	scene.init_time(TOTAL_TIME, fenetre)
 	add_child(scene)
 	main_timer.timeout.connect(scene.sync)
 	print("on ajoute une scene")
@@ -36,19 +40,23 @@ func _unhandled_input(event):
 		if event.pressed and event.keycode == KEY_SPACE:
 			var at_least_one_launched:bool = false;
 			# si c'est dans la fenetre on balance au scenes filles
-			if main_timer.time_left > fenetre and main_timer.time_left < 1 - fenetre :
-				game_over("out of rythm")
-			else :
+			if main_timer.time_left < fenetre or main_timer.time_left > 1 - fenetre :
 				for scene in list_space_scenes:
 					if scene.space_key_pressed():
 						at_least_one_launched = true
 				if (!at_least_one_launched):
 					game_over("no rocket to launch !")
+			else :
+				game_over("out of rythm")
 
 
 func on_rocket_launch():
 	print("Main receveive : rocket launched")
 	
+func on_rocket_crashed():
+	game_over("no rocket to launch !")
+	
+
 	
 func game_over(cause):
 	print ("GAME OVER : ", cause)
