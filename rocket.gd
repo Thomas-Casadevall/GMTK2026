d632440e2f96255475b75ac9623e9e0a422a0d57
Julien-Gio <julien.giovinazzo@gmail.com>
2026-07-26 11:00:45 +0200

    a bit of visuals goes a long way

--- rocket.gd
+++ rocket.gd
@@ -1,6 +1,12 @@
 extends Node2D
 
-const SPEED := 800
+signal construction_done
+@onready var constrution_order := [
+	$turbo,
+	$cabin,
+	$hat,
+]
+var construction_step := 0
 
 func _ready() -> void:
 	set_process(false)
@@ -10,7 +16,15 @@ func reset() -> void:
 	$AnimatedSprite2D.visible = false
 	$AnimatedSprite2D.pause()
 	position = Vector2.ZERO
+	construction_step = 0
+	constrution_order.map(func(s): s.visible = false)
 
 func start_smoke() -> void:
 	$AnimatedSprite2D.visible = true
 	$AnimatedSprite2D.play()
+
+func construct_step() -> void:
+	constrution_order[construction_step].visible = true
+	construction_step += 1
+	if construction_step >= constrution_order.size():
+		emit_signal("construction_done")
--- space_screen_background.gd
+++ space_screen_background.gd
@@ -2,28 +2,38 @@ extends Sprite2D
 
 # screenshake
 @onready var noise = FastNoiseLite.new()
-const SHAKE_DURATION := 0.4
+const SHAKE_DURATION := 0.5
 var noise_y = 0
 var shake_start: int = 0
-var amplitude := 0  # [0, 1]
-const decay := 0.8  # How quickly the shaking stops [0, 1].
-const max_offset := Vector2(100, 75)  # Maximum hor/ver shake in pixels.
-const max_roll := 0.1  # Maximum rotation in radians (use sparingly).
+const max_offset := Vector2(60, 7)  # Maximum hor/ver shake in pixels.
+const max_roll := 0.05  # Maximum rotation in radians (use sparingly).
+
+var original_pos: Vector2
 
 func _ready():
 	randomize()
+	noise.noise_type = FastNoiseLite.TYPE_PERLIN
 	noise.seed = randi()
 	noise.fractal_octaves = 2
+	original_pos = position
 	set_process(false)
 
 func _process(delta: float) -> void:
-	if Time.get_ticks_msec() > shake_start + SHAKE_DURATION * 1000:
+	var progress = (Time.get_ticks_msec() - shake_start) / (SHAKE_DURATION * 1000.0)
+	if progress >= 1:
+		rotation = 0
+		position = original_pos
 		set_process(false)
-	noise_y += 1
+		return
+	
+	var amplitude = 1 - progress
+	noise_y += 2
 	rotation = max_roll * amplitude * noise.get_noise_2d(noise.seed, noise_y)
-	offset.x = max_offset.x * amplitude * noise.get_noise_2d(noise.seed*2, noise_y)
-	offset.y = max_offset.y * amplitude * noise.get_noise_2d(noise.seed*3, noise_y)
-	amplitude -= delta * decay
+	var pos_offset = Vector2(
+		max_offset.x * amplitude * noise.get_noise_2d(noise.seed*2, noise_y),
+		max_offset.y * amplitude * noise.get_noise_2d(noise.seed*3, noise_y)
+	)
+	position = original_pos + pos_offset
 
 func shake() -> void:
 	shake_start = Time.get_ticks_msec()
