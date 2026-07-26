0acb38528eb74fce7e31cd543cfbf88e1de90bdb
Jamie Whitmarsh <88358457+GameAudioWizard@users.noreply.github.com>
2026-07-25 22:40:29 -0500

    Audio Adjustments
    Big adjustments to Music Player:
    
    MusicPlayer is now just an AudioStreamPlayer and plays the base music track
    CountdownPlayer is a class CountdownPlayer, which extends AudioStreamPlayer
    CountdownPlayer has the rhythms for the countdown. There are 6 total. If more than one rocket uses the same rhythm, then they should not trigger on_rocket_launch (which will make the layer silent again).
    
    Also, there are Music, SFX, and Countdown Busses in the Audio Mixer. This is in case we need to lower music volume for the countdown sounds to be louder.

--- layered_audio.gd
+++ layered_audio.gd
@@ -1,31 +1,27 @@
-class_name MusicPlayer
+class_name CountdownPlayer
 extends AudioStreamPlayer
 
 @export var rocket_times : Array[float]
 
-@onready var music_player : AudioStreamSynchronized = self.stream
+var volume_down : float = -60.0
+var volume_up : float = 0.0
 
-func _ready() -> void:
-	music_player.set_sync_stream_volume(0, 0.0) #setting the base track to 0 db, the others to silent
-	music_player.set_sync_stream_volume(1, -40)
-	music_player.set_sync_stream_volume(2, -40)
-	music_player.set_sync_stream_volume(3, -40)
-	music_player.set_sync_stream_volume(4, -40)
+@onready var countdown_player : AudioStreamSynchronized = self.stream
 
+func _ready() -> void: #set the countdown streams to inaudible
+	countdown_player.set_sync_stream_volume(0, volume_down)
+	countdown_player.set_sync_stream_volume(1, volume_down)
+	countdown_player.set_sync_stream_volume(2, volume_down)
+	countdown_player.set_sync_stream_volume(3, volume_down)
+	countdown_player.set_sync_stream_volume(4, volume_down)
+	countdown_player.set_sync_stream_volume(5, volume_down)
 
 func start_rocket_countdown(rocket_num : int) -> void:
 	var countdown_max : int = 8
 	var countdown_num : int = countdown_max
 	var stream_timing : float = rocket_times[rocket_num]
-
-	#get stream playback time, check the mod against it's duration, and once it hits a 0 mod go on
-	music_player.set_sync_stream_volume(rocket_num, 0) #brings the current rocket music track up
-	while countdown_num > countdown_max /2: #I suspect this section should be in the individual rocket's script, not here
-		# use a timer to count up to the time in rocket_times[rocket_num]
-		# countdown_num goes down by 1 each time the timer hits that 
-		pass
-	#once the countdown_num is half of the _max, start the rocket timer countdown (on screen numbers)
-	#player will press rocket launch button
+	countdown_player.set_sync_stream_volume(rocket_num, volume_up) #brings the current rocket music track up
+	#here we need to have the rocket's countdown number match the timing of the rhythm for the rocket_times - so if rhythm_1 is playing, the numbers should go down every 1.5 seconds
 
 func on_rocket_launch(rocket_num : int) -> void:
-	music_player.set_sync_stream_volume(rocket_num, -40) #reset the stream to silent
+	countdown_player.set_sync_stream_volume(rocket_num, volume_down) #reset the stream to silent
