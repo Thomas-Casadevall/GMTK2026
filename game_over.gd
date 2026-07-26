4e75c7921b69abe203843d9d1d1453b7df4ff792
Thomas-Casadevall <46565613+Thomas-Casadevall@users.noreply.github.com>
2026-07-26 14:47:49 +0200

    restart after game over

--- game_over.gd
+++ game_over.gd
@@ -1,11 +1,22 @@
 extends Node2D
 
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$CenterContainer/Label.text = GlobalVariables.game_over_reason
+	$mini_wait.start()
+	$mini_wait.wait_time = 1
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _unhandled_input(event):
+	if event is InputEventKey:
+		print ($mini_wait.time_left)
+		if $mini_wait.time_left != 0:
+			return
+		get_tree().change_scene_to_file("res://main_screen.tscn")
